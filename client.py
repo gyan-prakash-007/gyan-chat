cc6049db644191d19aef42b7f15d79660f6d8c95
gyan-prakash-007 <gyansir22@gmail.com>
2026-07-20 17:41:25 +0530

    Milestone 1: basic TCP connection - bind, listen, accept, connect working

---
+++ client.py
@@ -0,0 +1,9 @@
+import socket 
+
+HOST = '127.0.0.1'
+PORT = 65432
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((HOST,PORT))
+
+print("Connected to server")
---
+++ server.py
@@ -0,0 +1,13 @@
+import socket
+
+HOST = '127.0.0.1'
+PORT = 65432
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((HOST, PORT))
+server_socket.listen(5)
+
+print(f"Server listening on {HOST}:{PORT}")
+
+client_socket , client_address = server_socket.accept()
+print(f"new connection from {client_address}")
