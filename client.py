415644b0ba612073ffca3fc34ca4004db959840b
gyan-prakash-007 <gyansir22@gmail.com>
2026-07-20 18:25:23 +0530

    Add threading for multiple clients and message send/receive using protocol.py

--- client.py
+++ client.py
@@ -1,4 +1,6 @@
 import socket 
+import threading
+from protocol import pack_message, unpack_message
 
 HOST = '127.0.0.1'
 PORT = 65432
@@ -6,4 +8,18 @@ PORT = 65432
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((HOST,PORT))
 
-print("Connected to server")
+def receive_messages():
+    while True:
+        message = unpack_message(client_socket)
+        if message is None:
+            print('Disconnected from server')
+            break
+        print(f"\nReceived: {message}")
+
+receive_thread = threading.Thread(target=receive_messages)
+receive_thread.start
+
+while True:
+    text = input("enter a message")
+    data = {"type": "chat", "text":text}
+    client_socket.sendall(pack_message(data))
--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 import socket
+import threading
+from protocol import pack_message, unpack_message
 
 HOST = '127.0.0.1'
 PORT = 65432
@@ -9,5 +11,21 @@ server_socket.listen(5)
 
 print(f"Server listening on {HOST}:{PORT}")
 
-client_socket , client_address = server_socket.accept()
-print(f"new connection from {client_address}")
+def handle_client(client_socket, client_address):
+    print(f"Handling client {client_address}")
+
+    while True:
+        message = unpack_message(client_socket)
+        if message is None:
+            print(f"{client_address} disconnected")
+            client_socket.close()
+            break
+        print(f"Received from {client_address}: {message}")
+
+
+while True:
+    client_socket , client_address = server_socket.accept()
+    print(f"New connection from {client_address}")
+
+    thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
+    thread.start()
